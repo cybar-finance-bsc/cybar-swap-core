60b8c9b8b85dd0ea5c46bef3f4d78d0a3bd6dc40
inflation-man <zahl.eine@gmail.com>
2022-01-04 18:24:25 +0100

    Corrected the treasury fixture

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -7,6 +7,7 @@ import { expandTo18Decimals } from './utilities'
 import ERC20 from '../../build/ERC20.json'
 import CybarFactory from '../../build/CybarFactory.json'
 import CybarPair from '../../build/CybarPair.json'
+import CybarTreasury from '../../build/CybarTreasury.json'
 
 interface FactoryFixture {
   factory: Contract
@@ -44,7 +45,7 @@ export async function pairFixture(provider: Web3Provider, [wallet]: Wallet[]): P
   return { factory, token0, token1, pair }
 }
 
-export async function treasuryFixture(provider: Web3Provider, [wallet]: Wallet[]): Promise<TreasuryFixture> {
+export async function treasuryFixture(provider: Web3Provider, [wallet, lottery]: Wallet[]): Promise<TreasuryFixture> {
     
     const { factory } = await factoryFixture(provider, [wallet])
 
@@ -53,15 +54,24 @@ export async function treasuryFixture(provider: Web3Provider, [wallet]: Wallet[]
     const tokenB = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)], overrides)
 
     await factory.createPair(cybarToken.address, tokenA.address, overrides)
+    await factory.createPair(cybarToken.address, tokenB.address, overrides)
     await factory.createPair(tokenA.address, tokenB.address, overrides)
     const pairAddress0 = await factory.getPair(cybarToken.address, tokenA.address)
-    const pairAddress1 = await factory.getPair(tokenA.address, tokenB.address)
+    const pairAddress1 = await factory.getPair(cybarToken.address, tokenB.address)
+    const pairAddress2 = await factory.getPair(tokenA.address, tokenB.address)
     const pair0 = new Contract(pairAddress0, JSON.stringify(CybarPair.abi), provider).connect(wallet)
     const pair1 = new Contract(pairAddress1, JSON.stringify(CybarPair.abi), provider).connect(wallet)
+    const pair2 = new Contract(pairAddress2, JSON.stringify(CybarPair.abi), provider).connect(wallet)
 
     const token0Address = (await pair.token0()).address
     const token0 = tokenA.address === token0Address ? tokenA : tokenB
     const token1 = tokenA.address === token0Address ? tokenB : tokenA
 
-    return { factory, cybarToken, token0, token1, pair0, pair1 }
+    const treasury = await deployContract(wallet, CybarTreasury, [factory, cybarToken, lottery], overrides)
+
+    await treasury.addLPToken(pair0)
+    await treasury.addLPToken(pair1)
+    await treasury.addLPToken(pair2)
+
+    return { treasury, factory, cybarToken, token0, token1, pair0, pair1, pair2 }
 }
